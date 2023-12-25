87bdb78fa44dde8059545c0a3a256d4abadbb2e4
abhinav krishna v <96730888+abhinavkrishnav@users.noreply.github.com>
2023-12-25 22:03:35 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+// TeacherList.js
 
-function App() {
+import React from 'react';
+import './TeacherList.css'; // Import the CSS file for styling
+
+const TeacherList = () => {
+  const teachers = Array.from({ length: 28 }, (_, index) => ({
+    serialNo: index + 1,
+    teacherName: `Teacher ${index + 1}`,
+    uid: `UID${index + 1}`,
+  }));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="teacher-list-container">
+      <h1 className="page-heading">Teacher List</h1>
+      <table className="teacher-table">
+        <thead>
+          <tr>
+            <th>Serial No</th>
+            <th>Teacher Name</th>
+            <th>UID</th>
+          </tr>
+        </thead>
+        <tbody>
+          {teachers.map((teacher, index) => (
+            <tr key={index}>
+              <td>{teacher.serialNo}</td>
+              <td>{teacher.teacherName}</td>
+              <td>{teacher.uid}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
-}
+};
 
-export default App;
+export default TeacherList;
